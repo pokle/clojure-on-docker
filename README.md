e5e7e89272bee70264c69ff6a6c945993623f669
Tushar Pokle <tushar.pokle@gmail.com>
2013-12-16 20:44:11 +1100

    Now with a Compojure project

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 hello-compojure-world
 =====================
 
-A Clojure / Compojure hello world web server to help me try out some deployment tricks
+A Clojure / Compojure hello world web server to help me try out some deployment tricks.
+
+To run:
+
+    lein ring server
---
+++ src/hello_compojure_world/handler.clj
@@ -0,0 +1,12 @@
+(ns hello-compojure-world.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
